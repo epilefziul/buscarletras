9757efa60010bc0ba966c1e0be16bfda364efbca
luiz <luizfprofissional@gmail.com>
2019-12-02 17:06:52 -0300

    Implementando metodos de crud no controller de usuario;

--- src/letras/controller/UsuarioController.java
+++ src/letras/controller/UsuarioController.java
@@ -5,11 +5,53 @@ import letras.repository.UsuarioRepository;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
 import java.util.List;
 
 @ManagedBean
 @ViewScoped
-public class UsuarioController {
+public class UsuarioController implements Serializable {
+
+    private Usuario usuario = new Usuario();
+    private List<Usuario> usuarios;
+
+    public void salvar() {
+        new UsuarioRepository().cadastrar(usuario);
+    }
+
+    public void remover(Usuario usuarioDeletar) {
+        new UsuarioRepository().remover(usuarioDeletar);
+    }
+
+    public void alterar() {
+        new UsuarioRepository().alterar(usuario);
+    }
+
+    public List<Usuario> getListar() {
+        usuarios = new UsuarioRepository().listar();
+        return usuarios;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
     private final UsuarioRepository usuarioRepository;
     private Usuario usuario;
